7a93b5f2be841d41f893127ef2a709ae017438ad
Alexandro Perez <apgfriend@gmail.com>
2018-09-29 16:23:53 -0500

    Quick fix for gulp resonsive, so it handles other images besides jpg

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,7 +1,10 @@
 import gulp from 'gulp';
 import responsive from 'gulp-responsive';
+import del from 'del';
+import runSequence from 'run-sequence';
 
-gulp.task('images', function() {
+// Create responsive images for jpg files
+gulp.task('jpg-images', function() {
   return gulp.src('images/**/*.jpg')
     .pipe(responsive({
       // Resize all jpg images to three different sizes: 300, 600 and 800
@@ -20,4 +23,24 @@ gulp.task('images', function() {
       }]
     },))
     .pipe(gulp.dest('img/'));
+});
+
+// Just copy any other images to img folder
+gulp.task('other-images', function() {
+  return gulp.src(['!images/**/*.jpg', 'images/**/*.*'])
+    .pipe(gulp.dest('img/'));
+});
+
+// clean img folder
+gulp.task('clean', function(done) {
+  return del(['img/'], done);
+});
+
+// Run this task for your images.
+gulp.task("images", function(done) {
+  runSequence(
+    'clean',
+    ['jpg-images','other-images'],
+    done
+  );
 });
