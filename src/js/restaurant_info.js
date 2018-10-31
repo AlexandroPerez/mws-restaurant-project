64df8b0327609abf437fbc312d193b8886c2dcc6
Alexandro Perez <apgfriend@gmail.com>
2018-10-30 22:23:57 -0500

    Do Adding a form for new reviews, 3.4

--- src/js/restaurant_info.js
+++ src/js/restaurant_info.js
@@ -1,6 +1,7 @@
 import DBHelper from './dbhelper';
 import SECRET from './secret';
 import favoriteButton from "./favorite-button";
+import reviewForm from './review-form';
 //import './register-sw';
 
 let restaurant;
@@ -158,13 +159,19 @@ const fillReviewsHTML = (reviews = self.restaurant.reviews) => {
     const noReviews = document.createElement('p');
     noReviews.innerHTML = 'No reviews yet!';
     container.appendChild(noReviews);
-    return;
+  } else {
+    const ul = document.getElementById('reviews-list');
+    reviews.forEach(review => {
+      ul.appendChild(createReviewHTML(review));
+    });
+    container.appendChild(ul);
   }
-  const ul = document.getElementById('reviews-list');
-  reviews.forEach(review => {
-    ul.appendChild(createReviewHTML(review));
-  });
-  container.appendChild(ul);
+
+  const h3 = document.createElement('h3');
+  h3.innerHTML = "Leave a Review";
+  container.appendChild(h3);
+  const id = getParameterByName('id');
+  container.appendChild(reviewForm(id));
 }
 
 /**
